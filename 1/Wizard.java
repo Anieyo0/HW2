2c131df972f5d61f136e81ccf1ff30989a294007
Anieyo0 <106874538+Anieyo0@users.noreply.github.com>
2022-12-02 21:23:22 +0900

    Create Wizard.java

---
+++ 1/Wizard.java
@@ -0,0 +1,18 @@
+public class Wizard {
+
+    public Wizard(){
+        System.out.println('직업 : 마법사');
+    }
+
+    public void shot_fire_ball(){
+        System.out.println('파이어볼을 쏘는 중');
+    }
+
+    public void shield(){
+        System.out.println('마법방패를 소환하는 중');
+    }
+
+    public void portal(){
+        System.out.println('포탈을 소환하는 중');
+    }
+}
