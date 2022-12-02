6d10c360350e45b76257b62b9ca25ac8c431e75e
Anieyo0 <106874538+Anieyo0@users.noreply.github.com>
2022-12-02 21:29:24 +0900

    Update Fighter.java

--- 1/Fighter.java
+++ 1/Fighter.java
@@ -4,10 +4,16 @@ public class Fighter {
         System.out.println('Fighter 생성');
     }
 
-    public void attack(){  }
+    public void attack(){ 
+        System.out.println('주먹 휘두르는 중');
+    }
 
-    public void depend(){  }
+    public void depend(){ 
+        System.out.println('가드 올리는 중');
+    }
 
-    public void escape(){  }
+    public void escape(){ 
+        System.out.println('도망치는 중');
+    }
 
 }
